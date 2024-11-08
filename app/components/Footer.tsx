acdd3ed7605f2d488fd43cc8ea4f9577699e361f
NunzioBasciano <nunzio.basciano1988@gmail.com>
2024-11-08 17:47:36 +0100

    fix: in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,13 +5,16 @@ import { navLinks } from "../data/navLinks";
 
 function Footer() {
   return (
-    <footer className="bg-[var(--blue)] p-3">
+    <footer className="bg-[var(--blue)] p-3 fixed bottom-0 left-0 right-0">
       <nav>
         <ul className="flex items-center justify-evenly gap-3">
           {navLinks.map((item, index) => (
-            <li className="flex flex-col items-center gap-1" key={index}>
-              <Image src={item.image} alt={item.alt} width={25} height={25} />
-              <Link className="text-[10px]" href={item.link}>
+            <li key={index}>
+              <Link
+                className="text-[10px] flex flex-col items-center gap-1"
+                href={item.link}
+              >
+                <Image src={item.image} alt={item.alt} width={25} height={25} />
                 {item.label}
               </Link>
             </li>
