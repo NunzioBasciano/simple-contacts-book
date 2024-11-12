a69103006c02a7511a270975d8a988908a2ad210
NunzioBasciano <nunzio.basciano1988@gmail.com>
2024-11-12 19:16:04 +0100

    fix: css in Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,7 +7,7 @@ interface IHeaderProps {
 function Header(props: IHeaderProps) {
   const { label } = props;
   return (
-    <header className="bg-[var(--orange)] p-3 flex items-center justify-between">
+    <header className="bg-[var(--orange)] p-3 flex items-center justify-between fixed top-0 left-0 right-0">
       <h1 className="text-xl">{label}</h1>
     </header>
   );
