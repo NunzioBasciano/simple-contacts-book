cb21cce9de65afc35ed903ce2e1a563d2c7aa47b
NunzioBasciano <nunzio.basciano1988@gmail.com>
2024-11-13 22:34:38 +0100

    test commit

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,44 +7,23 @@ interface IButtonProps {
   imageAlt?: string;
   style?: string;
   action?: () => void;
-<<<<<<< HEAD
-  type?: "button" | "submit" | "reset"; // Aggiunto tipo per la sicurezza
-=======
   type?: "button" | "submit" | "reset";
->>>>>>> dev2
   validation?: boolean;
 }
 
 function Button(props: IButtonProps) {
-<<<<<<< HEAD
-  const {
-    label,
-    image,
-    imageAlt,
-    style,
-    action,
-    type = "button",
-    validation,
-  } = props; // Default type to 'button'
-
-  return (
-    <button
-      type={type}
-      onClick={action}
-      className={style}
-=======
   const { label, image, imageAlt, style, action, type, validation } = props;
   return (
     <button
       onClick={action}
       className={style}
       type={type}
->>>>>>> dev2
       disabled={validation}
     >
       {image && imageAlt && (
         <Image src={image} alt={imageAlt} width={20} height={20} />
       )}
+
       {label}
     </button>
   );
